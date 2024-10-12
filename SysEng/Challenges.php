ebaab4394ef409f44ff62f6336e46213ac1029b2
solemode404 <146377506+solemode404@users.noreply.github.com>
2024-10-12 13:44:52 -0400

    Add files via upload
    Php files

---
+++ SysEng/Challenges.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Challenges</title>
+	<link rel="stylesheet" href="./styles.css">
+</head>
+<?php include './home.php'; ?>
+
+<body>
+</body>
+</html>
---
+++ SysEng/home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Side Navigation Bar</title>
+	<link rel="stylesheet" href="./styles.css">
+	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="sidebar">
+        <h2>Habit U</h2>
+        <ul>
+            <li><a href="./habits.php"><i class="fas fa-home"></i>Habits</a></li>
+            <li><a href="./Notifications.php"><i class="fas fa-envelope"></i></i>Notifications</a></li>
+            <li><a href="./Challenges.php"><i class="fas fa-list"></i></i></i>Challenges</a></li>
+            <li><a href="./Planner.php"><i class="fas fa-layer-group"></i></i>Smart Fitness <br>Planner</a></li>
+            <li><a href="./Leaderboard.php"><i class="fas fa-list-ol"></i></i></i>Leaderboard</a></li>
+            <li><a href="./Register.php"><i class="fas fa-address-card"></i></i>Register</a></li>
+            <li><a href="./Login.php"><i class="fas fa-door-open"></i></i></i></i>Login</a></li>
+        </ul> 
+        <div class="logout">
+            <a href=""><i class="fas fa-door-closed"></i> Logout</a>
+        </div>
+        
+       
+    </div>
+   
+</div>
+
+</body>
+</html>
